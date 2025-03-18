d5e9812e5444374c9ae2c27f9b6f24efbad52194
Avadhut Noola <avdhutnula@gmail.com>
2025-03-18 14:27:49 +0530

    Add credit paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ function emojiCreator(emojiTerm) {
 function App() {
   return (
     <>
-      <h1>
-        <span> EmojiMeans </span>
-      </h1>
-
+      <h1> <span> EmojiMeans </span> </h1>
+      <p class="credit">
+        Created by <a href="https://www.linkedin.com/in/avadhut-noola/" target="_blank">Avadhut Noola</a> ❤️
+      </p>
       <dl className="dictionary">
         {emojipedia.map(emojiCreator)}
       </dl>
