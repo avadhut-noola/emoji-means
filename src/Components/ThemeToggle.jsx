c5b0f8634ea9ab83eed2a58d04d944dcd89857d9
Avadhut Noola <avdhutnula@gmail.com>
2025-03-20 22:02:35 +0530

    Add dark/light theme switching component

---
+++ src/Components/ThemeToggle.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ThemeToggle.css"; // Styles for toggle theme mode
+
+function ThemeToggle({ theme, toggleTheme }) {
+    return (
+      <div className="toggle-container">
+        <span className="toggle-text">{theme === "light" ? "Light Mode" : "Dark Mode"}</span>
+        <input
+          type="checkbox"
+          id="theme-toggle"
+          checked={theme === "dark"}
+          onChange={toggleTheme}
+        />
+        <label htmlFor="theme-toggle" className="toggle-label">
+          <span className="toggle-slider"></span>
+        </label>
+      </div>
+    );
+  }
+  
+  export default ThemeToggle;
